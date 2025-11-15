1b54d4d4fb8b0bcf104fdd9b5c701911f51bd908
gabriel-baptista-0607 <gabriel.baptista@ogfigovernance.com>
2025-11-15 11:45:00 -0300

    added include! Include is working!

---
+++ src/Escolha.asm
@@ -0,0 +1,68 @@
+TITLE Escolha jogador
+.MODEL SMALL
+.STACK 100h
+.DATA
+MSG1 DB 'Escolha a linha da jogada: $'
+MSG2 DB 'Escolha a coluna da jogada: $'
+.CODE 
+ JOGADOR PROC
+    PUSH AX
+    PUSH CX
+    PUSH DX
+    PUSH SI
+
+    MOV CX,2
+
+NOVAMENTE:
+    CMP CX,1
+    JE DOIS 
+    MOV AH,9
+    LEA DX,MSG1
+    INT 21h
+    JMP CAPTA
+
+DOIS:
+    MOV AH,9
+    LEA DX,MSG2
+    INT 21h
+    SHL BX,8
+
+CAPTA:
+    MOV AH,1
+    INT 21h
+
+    SUB AL,1
+    MOV BL,AL
+
+    
+
+    MOV AH,2
+    MOV DL,10
+    INT 21h
+
+    LOOP NOVAMENTE
+
+    PUSH BX
+    SHR BX, 8
+    AND BX, 0FH
+
+    POP SI
+    AND SI, 0FH
+
+    POP DI
+    POP DX
+    POP CX
+    POP AX
+    RET
+ JOGADOR ENDP
+
+ MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+
+    CALL JOGADOR
+
+    MOV AH,4Ch
+    INT 21h
+ MAIN ENDP
+END MAIN
