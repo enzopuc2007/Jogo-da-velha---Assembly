6ec74aa8fe7cbeb339caaace66a51f71be953f2a
gabriel-baptista-0607 <gabriel.baptista@ogfigovernance.com>
2025-11-17 14:57:21 -0300

    PROGRAM SIMPLIFIED

--- src/jogo.asm
+++ src/jogo.asm
@@ -1,16 +1,13 @@
 TITLE Bot
 .MODEL SMALL
 .STACK 100h
-; Warning !!!
-; Program is running well only in debug mode.
-; Please, execute in debug mode for this period of time.
 .DATA
   MSG_BEMVINDO DB 'JOGO DA VELHA', 13, 10, 13, 10, '$'
   MSG1 DB 'Selecione o modo de jogo (0 - Multiplayer | 1 - Computador)', 13, 10, 13, 10,'Digite a sua opcao: $'
   MSG2 DB 10,10,'Tente novamente, digito nao reconhecido...',13, 13, 10, '$'
-  ; MSG3 DB 'Insira o numero da linha em que você deseja inserir a sua peça do jogo (1 a 3): $', 13, 10
-  ; MSG4 DB 'Insira o numero da coluna em que você deseja inserir a sua peça do jogo (1 a 3): $', 13, 10
-  MATRIZ DB 3 DUP (3 DUP (?)) ; define a matriz de jogo da velha
+  MATRIZ DB 31H, 32H, 33H
+         DB 34H, 35H, 36H
+         DB 37H, 38H, 39H
   VETOR_G DB 9 DUP (?) ; [ , , , , , , , , ]
 
   MSG_ZERO DB 13, 10, 'A opcao selecionada foi a opcao MULTIPLAYER.', 13, 10, '$'
@@ -19,7 +16,7 @@ TITLE Bot
   MSG_INSIRA_LINHA DB 'Escolha a linha da jogada(Opcoes: 0, 3 ou 6): $'
   MSG_INSIRA_COLUNA DB 'Escolha a coluna da jogada(Opcoes: 0, 1 ou 2): $'
 
-  MSG_INVALIDO DB 'Posicao invalida. Tente novamente... $'
+  MSG_INVALIDO DB 'Posicao invalida. Tente novamente... $', 13, 10
 
   MSG_EMPATE DB 'Empate! $'
 
@@ -35,9 +32,7 @@ INCLUDE procs.inc
   VERIFICACAO_VIABILIDADE PROC
     ;PUSHALL 
 
-    CMP BX, 7h
-    JAE INVALIDO
-    CMP SI, 3H
+    CMP BX, 15
     JAE INVALIDO
     JMP FIM_VIABILIDADE
 
@@ -46,11 +41,11 @@ INCLUDE procs.inc
       LEA DX, MSG_INVALIDO
       INT 21H
 
-      MOV DL, 1
-      MOV CL, 2
+      ; MOV DL, 1
+      ; MOV CL, 2
 
   FIM_VIABILIDADE:
-      POP BX
+      ; POP BX
     ;POPALL
     RET
   VERIFICACAO_VIABILIDADE ENDP
@@ -58,60 +53,31 @@ INCLUDE procs.inc
   JOGO_MULTIPLAYER PROC ; procedimento de inicialização da opção multiplayer
 
   PUSHALL
-    ; MOV CH, 9
+  
+  MOV AH, 09H
+  MOV DX, OFFSET MSG_ZERO
+  INT 21h
 
-    MOV AH, 09H
-    MOV DX, OFFSET MSG_ZERO
-    INT 21H
-    ; PUSH CX ; 
-    MOV CH, 9
-    MOV CL, 2 ; 
+  MOV CH, 9
 
   NOVAMENTE:
     CALL IMPRIME_MATRIZ
 
-  LEITURA:
-  ; NOVAMENTE: ; le o endereco de linha
-    CMP CL, 1 ; 
-    JE DOIS ; 
-    
+  LEITURA:    
     MOV AH, 09H ; 
     LEA DX, MSG_INSIRA_LINHA ; 
     INT 21H ; 
 
     JMP CAPTA ; 
 
-  DOIS: ; le o endereco de coluna
-    MOV AH, 09H ;  
-    LEA DX, MSG_INSIRA_COLUNA ; 
-    INT 21H ;  
-    SHL BX, 8 ; 
-
   CAPTA: 
     MOV AH, 01H
     INT 21H
 
     MOV BL,AL
+    AND BL, 0FH
 
     PULA_LINHA
-
-    ; LOOP NOVAMENTE
-    DEC CL
-    JNZ LEITURA
-
-    PUSH BX
-    SHR BX, 8
-    AND BX, 7h
-
-    POP SI
-    AND SI, 3h
-
-    CALL VERIFICACAO_VIABILIDADE
-
-    CMP DX, 01H
-    JE LEITURA
-
-    ; POP CX
 
     VERIFICA_PARIDADE: ; SE CH FOR ÍMPAR -> VEZ DO J1
                        ; SE CH FOR PAR -> VEZ DO J2
@@ -122,14 +88,14 @@ INCLUDE procs.inc
       JZ IMPAR
 
       PAR:
-        MOV BYTE PTR MATRIZ[BX][SI], 6Fh
+        MOV BYTE PTR MATRIZ[BX], 6Fh
         ADD BX,SI
         MOV BYTE PTR VETOR_G[BX], 6Fh
         ; IMPRIME_MATRIZ
         JMP RETORNA_PRINCIPAL
 
       IMPAR:
-        MOV BYTE PTR MATRIZ[BX][SI], 78h
+        MOV BYTE PTR MATRIZ[BX], 78h
         ADD BX,SI
         MOV BYTE PTR VETOR_G[BX], 78h
         ; IMPRIME_MATRIZ
@@ -151,9 +117,6 @@ INCLUDE procs.inc
  MAIN PROC 
     MOV AX,@DATA      ;Inicialização dos dados
     MOV DS,AX
-
-
-    
 
     CALL INICIALIZACAO    ;Iniciação do jogo
 
